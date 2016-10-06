c9274cb394597e31af30a84e3eab27c8ca7d9af3
김병준 <kbj8952@naver.com>
2016-10-06 18:20:51 +0900

    설며 칸 : 테스트

---
+++ app/src/main/java/com/example/byoungjune/quickcoding2/MainActivity.java
@@ -0,0 +1,92 @@
+package com.example.byoungjune.quickcoding2;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    int a = 200;
+    int i = 250;
+    int count = 0;
+
+    public void ClickS(View v) {
+        if (a < i) {
+            Toast.makeText(getApplicationContext(), "Smaller", Toast.LENGTH_LONG).show();
+            i = i/2;
+            count = count+1;
+        }
+    }
+
+    public void ClickB(View v) {
+        if (a < i) {
+            Toast.makeText(getApplicationContext(), "Bigger", Toast.LENGTH_LONG).show();
+            i = i/2;
+            count = count+1;
+        }
+    }
+
+    public void ClickBG(View v) {
+
+            Toast.makeText(getApplicationContext(), "" + count, Toast.LENGTH_LONG).show();
+
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+}//주석
