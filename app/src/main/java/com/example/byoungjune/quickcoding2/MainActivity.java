11ce59d4afe77592bb8995e11d32a80b8ccba0b8
김병준 <kbj8952@naver.com>
2016-10-06 23:26:57 +0900

    퀵코딩2

--- app/src/main/java/com/example/byoungjune/quickcoding2/MainActivity.java
+++ app/src/main/java/com/example/byoungjune/quickcoding2/MainActivity.java
@@ -1,9 +1,10 @@
 package com.example.byoungjune.quickcoding2;
 
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.appindexing.Action;
@@ -18,40 +19,67 @@ public class MainActivity extends AppCompatActivity {
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
     private GoogleApiClient client;
+    private TextView var; //전역변수로 사용
+    private TextView var2;
+    private TextView var3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // ATTENTION: This was auto-generated to implement the App Indexing API.//random nextInt
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+        var = (TextView) findViewById(R.id.textView); // var라는 텍스트 뷰 변수를 만듦 id는 해당 id
+        var2 = (TextView) findViewById(R.id.textView2);
+        var3 = (TextView) findViewById(R.id.textView3);
+
+
     }
 
-    int a = 200;
-    int i = 250;
-    int count = 0;
+
+
+    int min =0;
+    int max =500;
+    int count =0;
+    int rd = 250;
+
 
     public void ClickS(View v) {
-        if (a < i) {
-            Toast.makeText(getApplicationContext(), "Smaller", Toast.LENGTH_LONG).show();
-            i = i/2;
-            count = count+1;
-        }
+
+
+        max = rd;
+
+
+        count = count+1;
+
+        var2.setText(max+"이하");
+        var3.setText(min+"이상");
+        rd = (int)(Math.random()*(max+1-min)+min);
+        var.setText("당신의 숫자는? : "+rd);// 텍스트메소드 호출
+
+
     }
 
     public void ClickB(View v) {
-        if (a < i) {
-            Toast.makeText(getApplicationContext(), "Bigger", Toast.LENGTH_LONG).show();
-            i = i/2;
-            count = count+1;
-        }
+
+        min = rd;
+
+
+        count = count+1;
+
+        var2.setText(max+"이하");
+        var3.setText(min+"이상");
+        rd = (int)(Math.random()*(max+1-min)+min);
+        var.setText("당신의 숫자는? : "+rd);
+
+
+
     }
 
     public void ClickBG(View v) {
 
-            Toast.makeText(getApplicationContext(), "" + count, Toast.LENGTH_LONG).show();
-
+        Toast.makeText(getApplicationContext(), "BingGo! "  + count +" 회의 시도끝에 맞췄습니다! ", Toast.LENGTH_LONG).show();
     }
 
     /**
